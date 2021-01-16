266588944f6ae112c6a5294e8c109566bfe0c217
Seth Russell <seth.ru4116@vusd.us>
2021-01-15 16:57:04 -0800

    This is the second commit

--- app/src/main/java/com/example/simpleasynctask/MainActivity.java
+++ app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private ProgressBar mProgressBar;
     private static final String TEXT_STATE = "currentText";
 
     @Override
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.textView1);
+        mProgressBar = findViewById(R.id.progressBar);
         if(savedInstanceState != null){
             mTextView.setText(savedInstanceState.getString(TEXT_STATE));
         }
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void startTask(View view) {
         mTextView.setText(R.string.napping);
-        new SimpleAsyncTask(mTextView).execute();
+        new SimpleAsyncTask(mTextView, mProgressBar).execute();
     }
 
     @Override
--- app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
+++ app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
@@ -1,16 +1,19 @@
 package com.example.simpleasynctask;
 
 import android.os.AsyncTask;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
 import java.util.Random;
 
-public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
+public class SimpleAsyncTask extends AsyncTask<Void, Integer, String> {
     private WeakReference<TextView> mTextView;
+    private WeakReference<ProgressBar> mProgressBar;
 
-    SimpleAsyncTask (TextView tv) {
+    SimpleAsyncTask (TextView tv, ProgressBar progressBar) {
         mTextView = new WeakReference<>(tv);
+        mProgressBar = new WeakReference<>(progressBar);
     }
 
     @Override
@@ -18,15 +21,24 @@ public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
         Random r = new Random();
         int n = r.nextInt(11);
         int s = n * 200;
-        try {
-            Thread.sleep(s);
-        } catch (InterruptedException e){
-            e.printStackTrace();
+        for(int i = 0; i < s; i+= 100) {
+            try {
+                Thread.sleep(100);
+                publishProgress((int) ((i + 100) / (float)s * 100));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if(isCancelled()) break;
         }
         return "Awake at last after sleeping for " + s + " milliseconds!";
     }
 
     protected void onPostExecute(String result){
         mTextView.get().setText(result);
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        mProgressBar.get().setProgress(values[0]);
     }
 }
